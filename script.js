d949cdbb79640982a2e676c537ae23f916dcffaf
GeekyHacks <geekyhacks22@gmail.com>
2023-04-30 03:32:55 +0300

    add fade effect and correct eslint&stylelint errors

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ var bodyContent = document.getElementById("bodyContent");
 // Get the button that opens the modal
 var menubtn = document.getElementById("menuBtn");
 
+//get menu options to use it for hidemenu function
+let menuItems = document.getElementsByClassName(".menuPageList > li a");
+
+menuItems.onclick = function () {
+  hideMenu();
+};
+
+function hideMenu() {
+  
+  modal.style.position = "fixed";
+  modal.style.background = "transparent";
+ 
+ modal.style.animation = "fadeOut 4s";
+ modal.style.display = "none";
+
+}
+
 // Get the x element that closes the modal
 var xclose = document.getElementsByClassName("xclose")[0];
 
@@ -19,10 +36,9 @@ menubtn.onclick = function () {
 
 // When the user clicks on  (x), close the modal
 function fadeOut() {
-  modal.style.animation = "fadeOut 1s";
-
+  modal.style.animation = "fadeOut 3s";
+  modal.style.display = "none";
 }
 xclose.onclick = function () {
   fadeOut();
-  modal.style.display = "none";
 };
