f3f6e5a04046b5d9704b57618061290c17b2fd9a
GeekyHacks <geekyhacks22@gmail.com>
2023-05-10 17:23:39 +0300

    edit index styling to suit popup page

--- script.js
+++ script.js
@@ -37,7 +37,15 @@ const projectPopup = document.querySelector("#popupPage");
 
 
 function popupPage() {
-  location.assign("popuppage.html");
+  let newWindow = open('/', 'example', 'width=300,height=300')
+  newWindow.focus();
+  
+  alert(newWindow.location.href); // (*) about:blank, loading hasn't started yet
+  
+  newWindow.onload = function() {
+    let html = `<div style="font-size:30px">Welcome!</div>`;
+    newWindow.document.body.insertAdjacentHTML('afterbegin', html);
+  };
 }
 
 function mainPage() {
