d15dfb34d961db51aa7be3066ba7160d93247bb2
GeekyHacks <geekyhacks22@gmail.com>
2023-05-10 12:26:03 +0300

    edit index styling

--- script.js
+++ script.js
@@ -15,28 +15,57 @@ const menuItems = document.querySelectorAll(".menuPageList > li a");
 // const bodyContent = document.getElementById('bodyContent');
 
 menubtn.addEventListener("click", () => {
-  modal.classList.toggle("show");
+  modal.classList.toggle("showMenu");
   modal.style.animation = "fadeIn 1s";
 });
 
 menuX.addEventListener("click", () => {
   modal.style.animation = "fadeOut 4s";
-  modal.classList.toggle("show");
+  modal.classList.toggle("showMenu");
 });
 
 menuItems.forEach((element) => {
   element.addEventListener("click", () => {
-    modal.classList.toggle("show");
+    modal.classList.toggle("showMenu");
   });
 });
-
 
 //popup page
 
 //get the popup page
-const projectPopup= document.querySelector('#projectPopup');
+const projectPopup = document.querySelector("#popupPage");
+
+
+function popupPage() {
+  location.assign("popuppage.html");
+}
+
+function mainPage() {
+  location.reload("index.html");
+}
 // Get the button that open popup page
-const projectBtn = document.querySelector("#projectBtn");
+const projectBtn = document.querySelectorAll("#projectBtn");
 
 // Get the x element that closes the modal
-const pageX = document.querySelector(".menuX");
+const pageX = document.querySelector(".pageX");
+
+// projectBtn.addEventListener("click", () => {
+//   const projectPopup = popupPage();
+//   projectPopup.style.animation = "fadeIn 1s";
+//   projectPopup.classList.toggle("showProject");
+// });
+
+projectBtn.forEach((element) => {
+  element.addEventListener("click", () => {
+    const projectPopup = popupPage();
+    projectPopup.style.animation = "fadeIn 5s";
+  
+  });
+});
+
+pageX.addEventListener("click", () => {
+  const mainPage = mainPage();
+  mainPage.style.animation = "fadeIn 1s";
+  mainPage.classList.toggle("showProject");
+
+});
