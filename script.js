fb0388e1a609f5650726adb324a1d73f402cfe00
GeekyHacks <geekyhacks22@gmail.com>
2023-05-23 11:26:40 +0300

    add inputform values to localstorage

--- script.js
+++ script.js
@@ -51,7 +51,6 @@ pageX.addEventListener('click', () => {
 });
 
 // the input valdiation
-
 function InvalidMsg(input) {
   if (input.value === '') {
     input.setCustomValidity('Entering an email-id is necessary!');
@@ -62,4 +61,26 @@ function InvalidMsg(input) {
   }
 
   return true;
+}
+
+// save input values to localstorage
+
+document.getElementById('name-input').value = getSavedValue('name-input'); // set the value to this input
+document.getElementById('email-input').value = getSavedValue('email-input');
+document.getElementById('textarea-input').value =
+  getSavedValue('textarea-input'); // set the value to this input
+
+//Save the value function - save it to localStorage as (ID, VALUE)
+function saveValue(e) {
+  var id = e.id; // get the sender's id to save it .
+  var val = e.value; // get the value.
+  localStorage.setItem(id, val); // Every time user writing something, the localStorage's value will override .
+}
+
+//get the saved value function - return the value of 'v' from localStorage.
+function getSavedValue(v) {
+  if (!localStorage.getItem(v)) {
+    return ''; // You can change this to your defualt value.
+  }
+  return localStorage.getItem(v);
 }
