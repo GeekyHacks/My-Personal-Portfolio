426367c153588d65d8f082598274ab220d1fc665
GeekyHacks <geekyhacks22@gmail.com>
2023-06-21 18:36:07 +0300

    Finish styling both desktop&mobile popup pages

--- JS/projects.js
+++ JS/projects.js
@@ -93,52 +93,178 @@ projectBtn.forEach((btn, btnIndex) => {
     projectPopup.style.display = 'block';
     const cardIndex = btnIndex;
     projectPopup.innerHTML = `
-      <div class="popup">
-        <h2 class="pop-title">${projectsarray[cardIndex].Name}</h2>
-        <ul class="pop-field">
-          <li class="cano">CANOPY</li>
-          <li class="tag">&#8226; Back End Dev</li>
-          <li class="tag">&#8226; 2015</li>
+    <div class="popuppage-header">
+    <img
+      class="pageX"
+      src="assests/images/x.svg"
+      alt="close"
+    />
+    <img
+      src="${projectsarray[cardIndex].img}"
+      alt="Medical Illustration Sets"
+      class="poppage-desktop-img"
+    />
+  </div>
+  <div class="poppage">
+  <div class="desktop-content">
+      <div class="first-section">
+        <h1 class="title-desktop">
+        ${projectsarray[cardIndex].Name}
+        </h1>
+        <ul class="poppage-buttons">
+          <li>
+            <a href="${projectsarray[cardIndex].liveVersion}" tabindex="0">
+              See live<img
+                src="assests/images/see live icon.svg"
+                alt="see live"
+            /></a>
+          </li>
+          <li>
+            <a
+              href="${projectsarray[cardIndex].sourecLink}"
+              tabindex="0"
+            >
+              See Source<img
+                src="assests/images/git.svg"
+                alt="project source code"
+              />
+            </a>
+          </li>
         </ul>
-        <div class="pop-img-container">
-          <img class="pop-img" src="${projectsarray[cardIndex].img}" alt="">
-        </div>
-        <div class="pop-para">
-          <p>${projectsarray[cardIndex].description[0]}</p>
-        </div>
-        <div class="pop-skills">
-          <ul class="pop-training">
-            <li class="other-skills">HTML</li>
-            <li class="other-skills">CSS</li>
-            <li class="other-skills">JavaScript</li>
-            <li class="other-skills">Ruby</li>
-            <li class="other-skills">Bootstrap</li>
-          </ul>
-        </div>
-        <div class="pop-links">
-          <a href="${projectsarray[cardIndex].liveVersion}">
-            <button class="pop-link1 pop-card-btn" type="submit">
-              See live
-              <img class="search" src="pop-images/live.png" alt="">
-            </button>
-          </a>
-          <a href="${projectsarray[cardIndex].sourecLink}">
-            <button class="pop-link2 pop-card-btn" type="submit">
-              See source
-              <img class="search" src="pop-images/popup.png" alt="">
-            </button>
-          </a>
-        </div>
-        <p id="close-btn">&times;</p>
       </div>
+      <ul class="labels">
+        <li>Codekit</li>
+        <li>GitHub</li>
+        <li>JavaScript</li>
+        <li>Bootstrap</li>
+        <li>Terminal</li>
+        <li>Codepen</li>
+      </ul>
+      <p class="p-1">
+        Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry. Lorem industry's standard dummy
+        text ever since the 1500s, when an unknown printer
+        took a galley of type and scrambled it 1960s.
+      </p>
+    </div>
+
+    <div class="mobile-content">
+      <h1 class="title-mobile">
+      ${projectsarray[cardIndex].Name}
+      </h1>
+      <ul class="labels">
+        <li>Ruby and Rails</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+      </ul>
+      <p class="p-1">
+        Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and
+        scrambled it 1960s.
+      </p>
+      <p class="p-2">
+        Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and
+        scrambled it 1960s.
+      </p>
+      <ul class="poppage-buttons">
+        <li>
+          <a href="${projectsarray[cardIndex].liveVersion}">
+            See live
+            <img
+              src="assests/images/see live icon.svg"
+              alt="see live"
+          /></a>
+        </li>
+        <li>
+          <a
+            href="${projectsarray[cardIndex].sourecLink}"
+          >
+            See Source
+            <img
+              src="assests/images/git.svg"
+              alt="project source code"
+            />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
     `;
 
-    const closebtn = document.querySelector('#close-btn');
+    const closebtn = document.querySelector('.pageX');
     closebtn.addEventListener('click', () => {
       projectPopup.style.display = 'none';
     });
   });
 });
+
+
+
+// content
+// <div class="popup">
+// <img
+// class="pageX"
+// src="assests/images/x.svg"
+// alt="close"
+// />
+// <div class="pop-img-container">
+// <img class="pop-img" src="${projectsarray[cardIndex].img}" alt="">
+// </div>
+
+//   <h2 class="pop-title">${projectsarray[cardIndex].Name}</h2>
+//   <ul class="pop-field">
+//     <li class="cano">CANOPY</li>
+//     <li class="tag">&#8226; Back End Dev</li>
+//     <li class="tag">&#8226; 2015</li>
+//   </ul>
+
+//   <div class="pop-para">
+//     <p>${projectsarray[cardIndex].description[0]}</p>
+//   </div>
+//   <div class="pop-skills">
+//     <ul class="pop-training">
+//       <li class="other-skills">HTML</li>
+//       <li class="other-skills">CSS</li>
+//       <li class="other-skills">JavaScript</li>
+//       <li class="other-skills">Ruby</li>
+//       <li class="other-skills">Bootstrap</li>
+//     </ul>
+//   </div>
+//   <div class="pop-links">
+//     <a href="${projectsarray[cardIndex].liveVersion}">
+//       <button class="pop-link1 pop-card-btn" type="submit">
+//         See live
+//         <img class="search" src="pop-images/live.png" alt="">
+//       </button>
+//     </a>
+//     <a href="${projectsarray[cardIndex].sourecLink}">
+//       <button class="pop-link2 pop-card-btn" type="submit">
+//         See source
+//         <img class="search" src="pop-images/popup.png" alt="">
+//       </button>
+//     </a>
+//   </div>
+  
+// </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function createProjects() {
 //   const projects = document.querySelector('.projectsContainer');
