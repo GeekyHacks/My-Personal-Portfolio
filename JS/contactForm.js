01de9e696c1379a835ed6291a06e8625dae727db
GeekyHacks <geekyhacks22@gmail.com>
2023-06-23 13:23:42 +0300

    Finish Required Changes

--- JS/contactForm.js
+++ JS/contactForm.js
@@ -1,45 +1,24 @@
 // The form section
-const submitBtn = document.querySelector('#submitBtn');
-
-// Save the value function - save it to localStorage as (ID, VALUE)
-// function saveValue(e) {
-//   const { id, val } = e; // destructure
-//   // const id = e.id; // get the sender's id to save it .
-//   // const val = e.value; // get the value.
-//   // Every time user writing something, the localStorage's value will override .
-//   localStorage.setItem(id, val);
-// }
-
-// get the saved value function - return the value of 'v' from localStorage.
-// function getSavedValue(v) {
-//   if (!localStorage.getItem(v)) {
-//     return saveValue(); // You can change this to your defualt value.
-//   }
-//   return localStorage.getItem(v);
-// }
-
-// // save input values to localstorage
-// document.getElementById('firstname-input').value = getSavedValue('firstname-input');
-// document.getElementById('secondname-input').value = getSavedValue('secondname-input');
-// set the value to this input
-// document.getElementById('email-input').value = getSavedValue('email-input');
-// document.getElementById('textarea-input').value = getSavedValue('textarea-input');
 
 // the input valdiation
-function InvalidMsg(input) {
-  if (input.value === '') {
-    input.setCustomValidity('Entering an email-id is necessary!');
-  } else if (input.validity.patternMismatch) {
-    input.setCustomValidity('Please enter a correct lowercase email address!');
-  } else {
-    input.setCustomValidity('');
+const contactForm = document.querySelector('.inputForm');
+const emailPattern = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+\.[a-z]{2,3}/;
+
+contactForm.addEventListener('submit', function (event) {
+  const UserInput = document.getElementById('email-input');
+  const vaild = emailPattern.test(UserInput.value);
+  const errorMsg = document.getElementById('errorMessage');
+
+  if (!vaild) {
+    errorMsg.style.height = '20px';
+    errorMsg.style.background = '#FFD600';
+    errorMsg.style.borderRadius = '5px';
+    errorMsg.style.alignSelf = 'center';
+    errorMsg.style.padding = '5px';
+    errorMsg.innerHTML = '*Email must be in email format (abd@sdm.com) and in lowercase letters!';
+    event.preventDefault();
+    UserInput.value = '';
+    UserInput.focus();
+    return;
   }
-
-  return true;
-}
-
-submitBtn.addEventListener('click', () => {
-  // getSavedValue();
-  InvalidMsg();
-  // saveValue();
 });
